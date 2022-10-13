eaea30435e4bb6fb0c457b66c98aa8b52d20933c
Elijah <ademosukingsley@gmail.com>
2022-10-13 16:35:47 -0700

    The mighty commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
+ */
+#
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This contains solutions to hello_world
