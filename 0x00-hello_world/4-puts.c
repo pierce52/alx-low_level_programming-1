aec8ab09a1414b1509c82136acca5a77b22e0ad6
Elijah <ademosukingsley@gmail.com>
2022-10-13 17:04:15 -0700

    the commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main- Serves as the entry point for the program
  *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * Return: Return: Always 0 (Success)
  */
-#
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
